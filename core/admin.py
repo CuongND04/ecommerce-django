16dc336e0ecfebafbe31d275e1e3be36259a7f7a
Nguyễn Đức Cường <uteu237@gmail.com>
2024-09-15 21:46:51 +0700

    xây dựng model

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from core.models import CartOrderItems,  Product, Category, Vendor, CartOrder, ProductImages, ProductReview, WishList, Address
+# Cài đặt các trường hiển thị trong admin của từng model
+class ProductImagesAdmin(admin.TabularInline):
+    model = ProductImages
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImagesAdmin]
+    list_display = ['user', 'title', 'product_image', 'price','featured', 'product_status']
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category_image']
+
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['title', 'vendor_image']
+
+
+class CartOrderAdmin(admin.ModelAdmin):
+    list_display = ['user',  'price', 'paid_status', 'order_date','product_status']
+
+
+class CartOrderItemsAdmin(admin.ModelAdmin):
+    list_display = ['order', 'invoice_no', 'item', 'image','qty', 'price', 'total']
+
+
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'review', 'rating']
+
+
+class WishListAdmin(admin.ModelAdmin):
+    list_display = ['user', 'product', 'date']
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'address', 'status']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Vendor, VendorAdmin)
+admin.site.register(CartOrder, CartOrderAdmin)
+admin.site.register(CartOrderItems, CartOrderItemsAdmin)
+admin.site.register(ProductReview, ProductReviewAdmin)
+admin.site.register(WishList, WishListAdmin)
+admin.site.register(Address, AddressAdmin)
