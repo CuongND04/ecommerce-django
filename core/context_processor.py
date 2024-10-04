225881e7043a43f4ba77d8de4d119e98eec16a93
Nguyễn Đức Cường <uteu237@gmail.com>
2024-10-04 16:40:26 +0700

    lọc sản phẩm theo giá

--- core/context_processor.py
+++ core/context_processor.py
@@ -1,10 +1,15 @@
 from core.models import CartOrderItems,  Product, Category, Vendor, CartOrder, ProductImages, ProductReview, WishList, Address
-
+from django.db.models import Min,Max
 
 def default(request):
   categories  = Category.objects.all()
   vendors = Vendor.objects.all()
+
+  min_max_price = Product.objects.aggregate(Min("price"),Max("price"))
+
+
   return {
     "categories":categories,
-    "vendors":vendors
+    "vendors":vendors,
+    "min_max_price":min_max_price,
   }
